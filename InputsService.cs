c21eb20dcb8add8d206239bac1f8ea339b4f4017
d-fin <dfinley5656@gmail.com>
2023-04-22 10:42:47 -0400

    adding files from exisiting project.

---
+++ InputsService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Sealevel; 
+
+namespace SensorInputsService
+{
+    public partial class InputsService : ServiceBase
+    {
+        //variables 
+        SeaMAX sm = new SeaMAX();
+
+
+        public InputsService()
+        {
+            InitializeComponent();
+        }
+
+        protected void WorkProcess()
+        {
+            int errno = sm.SDL_Initialize();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.WriteLog("Starting Service");
+        }
+
+        protected override void OnStop()
+        {
+            int errno = sm.SDL_Cleanup();
+            Logger.WriteLog("Stopping Service");
+        }
+    }
+}
